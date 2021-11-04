6eaee13ca9c9e0ef92a9bbbac5dfbdda1a6d1e29
raassh-23 <muh.nur.abdurrauf@gmail.com>
2021-11-04 21:14:47 +0700

    Add game project

---
+++ Assets/Scripts/ResourceController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceController : MonoBehaviour
+{
+    public Button ResourceButton;
+    public Image ResourceImage;
+    public Text ResourceDescription;
+    public Text ResourceUpgradeCost;
+    public Text ResourceUnlockCost;
+
+    private ResourceConfig _config;
+
+    private int _level = 1;
+
+    public bool isUnlocked { get; private set; }
+
+    private void Start() {
+        ResourceButton.onClick.AddListener(() => {
+            if (isUnlocked) {
+                UpgradeLevel();
+            } else {
+                UnlockResource();
+            }
+        });
+    }
+
+    public void SetConfig(ResourceConfig config)
+    {
+        _config = config;
+
+        ResourceDescription.text = $"{_config.Name} Lv. {_level}\n+{GetOutput().ToString("0")}";
+        ResourceUnlockCost.text = $"Unlock Cost\n{GetUnlockCost()}";
+        ResourceUpgradeCost.text = $"Upgrade Cost\n{GetUpgradeCost()}";
+
+        SetUnlocked(_config.UnlockCost == 0);
+    }
+
+    public double GetOutput()
+    {
+        return _config.Output * _level;
+    }
+
+    public double GetUpgradeCost()
+    {
+        return _config.UpgradeCost * _level;
+    }
+
+    public double GetUnlockCost()
+    {
+        return _config.UnlockCost;
+    }
+
+    public void UpgradeLevel() 
+    {
+        double upgradeCost = GetUpgradeCost();
+
+        if(GameManager.Instance.TotalGold < upgradeCost)
+        {
+            return;
+        }
+
+        GameManager.Instance.AddGold(-upgradeCost);
+        _level++;
+
+        ResourceUpgradeCost.text = $"Upgrade Cost\n{GetUpgradeCost()}";
+        ResourceDescription.text = $"{_config.Name} Lv. {_level}\n+{GetOutput().ToString("0")}";
+    }
+
+    public void UnlockResource()
+    {
+        double unlockCost = GetUnlockCost();
+
+        if(GameManager.Instance.TotalGold < unlockCost)
+        {
+            return;
+        }
+
+        GameManager.Instance.AddGold(-unlockCost);
+        isUnlocked = true;
+
+        SetUnlocked(true);
+        GameManager.Instance.ShowNextResource();
+
+        AchievementController.Instance.UnlockAchievement(AchievementType.UnlockResource, _config.Name);
+    }
+
+    public void SetUnlocked(bool unlocked)
+    {
+        isUnlocked = unlocked;
+        ResourceImage.color = isUnlocked ? Color.white : Color.grey;
+        ResourceUnlockCost.gameObject.SetActive(!unlocked);
+        ResourceUpgradeCost.gameObject.SetActive(unlocked);
+    }
+}
